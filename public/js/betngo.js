b14892bc2a07ea06435c773ccf9831cd565bf3d4
Julian Luco Contestin <julianluco@gmail.com>
2019-11-16 19:06:24 -0300

    agrego juego, mejor out y mejoras varias en flujo y maqueta

--- public/js/betngo.js
+++ public/js/betngo.js
@@ -2,12 +2,13 @@ new Vue({
     el: '#betngo',
 
     data: {
+        jackpotAcumulado: 510950,
         nombreJugador: 'Pepito Doe',
         creditoInicio: 50000,
-        partidasJugadas: 0,
-        partidasGanadas: 0,
+        partidasJugadas: 105,
+        partidasGanadas: 15,
         jackpots: 0,
-        creditoAcumulado: 50000,
+        creditoAcumulado: 61541,
     },
 
     methods: {
