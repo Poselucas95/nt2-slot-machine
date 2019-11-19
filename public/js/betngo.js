c653e004e184eb794ca010061ec80e3fedaa36d7
ATENCIO MATIAS ARIEL <35426807@comunidadort.edu.ar>
2019-11-19 02:55:26 -0300

    Extension de funcionalidades

--- public/js/betngo.js
+++ public/js/betngo.js
@@ -5,6 +5,126 @@ const next = window.requestAnimationFrame ||
    window.oRequestAnimationFrame ||
    function(cb) { window.setTimeout(cb, 1000/60) }
 
+const juego1 = [
+    {
+        nombre: 'pera',
+        imagen: 'icons/pera.png',
+        valor: 1,
+        esJackpot: false
+    },
+    {
+        nombre: 'sandia',
+        imagen: 'icons/sandia.png',
+        valor: 2,
+        esJackpot: false
+    },
+    {
+        nombre: 'palta',
+        imagen: 'icons/palta.png',
+        valor: 3,
+        esJackpot: false
+    }
+]
+
+const juego2 = [
+    {
+        nombre: 'pica',
+        imagen: 'icons/pica.png',
+        valor: 1,
+        esJackpot: false
+    },
+    {
+        nombre: 'corazon',
+        imagen: 'icons/cora.png',
+        valor: 2,
+        esJackpot: false
+    },
+    {
+        nombre: 'diamante',
+        imagen: 'icons/diamante.png',
+        valor: 3,
+        esJackpot: false
+    },
+    {
+        nombre: 'perejil',
+        imagen: 'icons/perejil.png',
+        valor: 4,
+        esJackpot: false
+    },
+]
+
+const juego3 = [
+    {
+        nombre: 'tercero1',
+        imagen: 'icons/tercero1.png',
+        valor: 1,
+        esJackpot: false
+    },
+    {
+        nombre: 'tercero2',
+        imagen: 'icons/tercero2.png',
+        valor: 2,
+        esJackpot: false
+    },
+    {
+        nombre: 'tercero3',
+        imagen: 'icons/tercero3.png',
+        valor: 3,
+        esJackpot: false
+    },
+    {
+        nombre: 'tercero4',
+        imagen: 'icons/tercero4.png',
+        valor: 4,
+        esJackpot: false
+    },
+    {
+        nombre: 'tercero5',
+        imagen: 'icons/tercero5.png',
+        valor: 5,
+        esJackpot: false
+    }
+]
+
+const juego4 = [
+    {
+        nombre: 'chifei1',
+        imagen: 'icons/chifei1.png',
+        valor: 100,
+        esJackpot: false
+    },
+    {
+        nombre: 'chifei2',
+        imagen: 'icons/chifei2.png',
+        valor: 200,
+        esJackpot: false
+    },
+    {
+        nombre: 'chifei3',
+        imagen: 'icons/chifei3.png',
+        valor: 300,
+        esJackpot: false
+    },
+    {
+        nombre: 'chifei4',
+        imagen: 'icons/chifei4.png',
+        valor: 400,
+        esJackpot: false
+    },
+    {
+        nombre: 'chifei5',
+        imagen: 'icons/chifei5.png',
+        valor: 500,
+        esJackpot: false
+    },
+    {
+        nombre: 'chifei6',
+        imagen: 'icons/chifei6.png',
+        valor: 600,
+        esJackpot: true
+    }
+]
+
 new Vue({
     el: '#betngo',
 
@@ -16,40 +136,33 @@ new Vue({
         partidasGanadas: 0,
         jackpots: 0,
         creditoAcumulado: 0,
-        slots: [{
-            title: "When",
+        tamanioApuesta: 0,
+        juego: [],
+        slots: [
+          {
             items: [
-                "today",
-              "next week",
-              "last year",
-              "tomorrow",
-              "yesterday",
+              juego1[0].imagen,
+              juego1[1].imagen,
+              juego1[2].imagen,
             ]
           }, 
           {
-            title: "Where",
             items: [
-                "at home",
-              "at work",
-              "at school",
-              "at the gym",
-              "at the park",
-              "at the beach",
-              "at the sidewalk",
-              "at the city",
+                juego1[0].imagen,
+                juego1[1].imagen,
+                juego1[2].imagen,
             ]
           }, 
           {
-            title: "How",
             items: [
-            "cycling",
-              "walking",
-              "swimming",
-              "flying",
+                juego1[0].imagen,
+                juego1[1].imagen,
+                juego1[2].imagen,
             ]
           }],
         opts: null,
         startedAt: null,
+        elegidos: []
     },
     methods: {
         saludar: function () {
@@ -60,12 +173,19 @@ new Vue({
         confirmarImporte: function () {
             this.creditoAcumulado = this.creditoInicio;
         },
+        setTamanioApuesta: function (tamanio) {
+            this.tamanioApuesta = tamanio;
+            this.start;
+        },
         start: function() {
-        
+            this.elegidos = []
             if (this.opts) {
                 return
             }
-                        
+            
+            this.creditoAcumulado -= this.tamanioApuesta;
+            this.partidasJugadas += 1;
+            
             this.opts = this.slots.map( (data, i) => {
             
                 const slot = this.$refs.slots[i]
@@ -80,15 +200,14 @@ new Vue({
                 duration: 3000 + i * 700, // milliseconds
                 isFinished: false,
                 }
-                
+                this.elegidos.push(this.juego[choice])
                 return opts
                 
             })
-        
             next( this.animate )
         }, 
         animate: function(timestamp) {
-    
+            
             if (this.startedAt == null) {
                 this.startedAt = timestamp
             }
@@ -121,10 +240,66 @@ new Vue({
                 this.opts = null
                 this.startedAt = null
                 console.log('finished')
+                console.log(this.elegidos)
+                this.darPremio();
             } 
             else {
                 next( this.animate )
             }
+        },
+        darPremio: function() {
+
+            //Gana sin jackpot
+            if(this.elegidos[0].nombre == this.elegidos[1].nombre  && this.elegidos[1].nombre == this.elegidos[2].nombre && !this.elegidos[0].esJackpot)
+            {
+                this.creditoAcumulado += this.tamanioApuesta * this.elegidos[0].valor;
+                this.partidasGanadas += 1;
+                this.jackpotAcumulado += this.tamanioApuesta / 7;
+                alert('GANASTE $ ' + this.tamanioApuesta * this.elegidos[0].valor)
+            }
+            else if(this.elegidos[0].nombre == this.elegidos[1].nombre  && this.elegidos[1].nombre == this.elegidos[2].nombre && this.elegidos[0].esJackpot)
+            {
+                this.creditoAcumulado += this.jackpotAcumulado;
+                this.partidasGanadas += 1;
+                this.jackpots += 1;
+                this.jackpotAcumulado = 5000;
+                alert('FELICITACIONES!!! HAZ GANADO EL JACKPOT ACUMULADO DE $ ' + this.jackpotAcumulado)                
+            }
+            console.log("premio: " + this.creditoAcumulado)
+            console.log(this.elegidos[0].nombre == this.elegidos[1].nombre == this.elegidos[2].nombre);
+            console.log("tamanio apuesta: " + this.tamanioApuesta)
+            console.log(this.elegidos[0].nombre+ ' ' + this.elegidos[1].nombre + ' ' + this.elegidos[2].nombre);
+            console.log(this.juego);
+        },
+        elegirJuego: function(numeroJuego) {
+            let iconos;
+            let juegoNuevo;
+            switch(numeroJuego)
+            {
+                case 1:
+                    iconos = [juego1[0].imagen,juego1[1].imagen,juego1[2].imagen];
+                    juegoNuevo = [juego1[0],juego1[1],juego1[2]];
+                    break;
+                case 2:
+                    iconos = [juego2[0].imagen,juego2[1].imagen,juego2[2].imagen,juego2[3].imagen];
+                    juegoNuevo = [juego2[0],juego2[1],juego2[2],juego2[3]];
+                    break;
+                case 3:
+                    iconos = [juego3[0].imagen,juego3[1].imagen,juego3[2].imagen,juego3[3].imagen,juego3[4].imagen];
+                    juegoNuevo = [juego3[0],juego3[1],juego3[2],juego3[3],juego3[4]];
+                    break;
+                case 4:
+                    iconos = [juego4[0].imagen, juego4[1].imagen, juego4[2].imagen, juego4[3].imagen, juego4[4].imagen, juego4[5].imagen];
+                    juegoNuevo = [juego4[0],juego4[1],juego4[2],juego4[3],juego4[4],juego4[5]]
+                }
+            this.slots[0].items = iconos;
+            this.slots[1].items = iconos;
+            this.slots[2].items = iconos;
+            this.juego = juegoNuevo;
+            console.log("asdasd");
+            console.log(iconos);
+            console.log(this.slots);
+            return iconos
         } 
         
     }
--- public/js/template.js
+++ public/js/template.js
@@ -21,11 +21,45 @@ $("#juego1").click(function () {
 	$("#block4").show();
 });
 
-// prueba
-
-$("#jugar").click(function () {
+$("#juego2").click(function () {
 	$("#block3").hide();
 	$("#block4").show();
+});
+
+$("#juego3").click(function () {
+	$("#block3").hide();
+	$("#block4").show();
+});
+
+$("#juego4").click(function () { //pagina de slots
+	$("#block3").hide();
+	$("#block4").show();
+});
+
+$("#imgLogo").click(function () {
+	$("#block1").hide();
+	$("#block2").hide();
+	$("#block3").hide();
+	$("#block4").hide();
+	$("#block0").show();
+});
+
+//Botones x10 x50 x100
+
+$("#ap10").click(function () {
+	$("#ap10").css("background-color", "orange");
+	$("#ap50").css("background-color", "black");
+	$("#ap100").css("background-color", "black");
+});
+$("#ap50").click(function () {
+	$("#ap10").css("background-color", "black");
+	$("#ap50").css("background-color", "orange");
+	$("#ap100").css("background-color", "black");
+});
+$("#ap100").click(function () {
+	$("#ap10").css("background-color", "black");
+	$("#ap50").css("background-color", "black");
+	$("#ap100").css("background-color", "orange");
 });
 
 $("#salir").click(function () {
