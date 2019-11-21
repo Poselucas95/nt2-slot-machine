a3adb55373faf28292fd5e0f5552fc6486dee47d
lpose <lucas.pose@mercadolibre.com>
2019-11-21 12:34:42 -0300

    Se disablea el boton en caso de tener 0.

--- public/js/betngo.js
+++ public/js/betngo.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-new */
+/* eslint-disable no-undef */
+/* eslint-disable indent */
 const next = window.requestAnimationFrame ||
    window.webkitRequestAnimationFrame ||
    window.mozRequestAnimationFrame ||
    window.msRequestAnimationFrame ||
    window.oRequestAnimationFrame ||
-   function(cb) { window.setTimeout(cb, 1000/60) }
+   function (cb) { window.setTimeout(cb, 1000 / 60) }
 
 const juego1 = [
     {
@@ -50,7 +53,7 @@ const juego2 = [
         imagen: 'icons/perejil.png',
         valor: 80,
         esJackpot: false
-    },
+    }
 ]
 
 const juego3 = [
@@ -143,21 +146,21 @@ new Vue({
             items: [
               juego1[0].imagen,
               juego1[1].imagen,
-              juego1[2].imagen,
+              juego1[2].imagen
             ]
-          }, 
+          },
           {
             items: [
                 juego1[0].imagen,
                 juego1[1].imagen,
-                juego1[2].imagen,
+                juego1[2].imagen
             ]
-          }, 
+          },
           {
             items: [
                 juego1[0].imagen,
                 juego1[1].imagen,
-                juego1[2].imagen,
+                juego1[2].imagen
             ]
           }],
         opts: null,
@@ -168,139 +171,127 @@ new Vue({
         saludar: function () {
             // metodo usado para testear
             alert('Texto: ' + this.nombreJugador)
-            
         },
         confirmarImporte: function () {
-            this.creditoAcumulado = this.creditoInicio;
+            this.creditoAcumulado = this.creditoInicio
         },
         setTamanioApuesta: function (tamanio) {
-            this.tamanioApuesta = tamanio;
-            this.start;
+            this.tamanioApuesta = tamanio
+            this.start
         },
-        start: function() {
+        start: function () {
             this.elegidos = []
             if (this.opts) {
                 return
             }
-            
-            this.creditoAcumulado -= this.tamanioApuesta;
-            this.partidasJugadas += 1;
-            
-            this.opts = this.slots.map( (data, i) => {
-            
+
+            this.creditoAcumulado -= this.tamanioApuesta
+            this.partidasJugadas += 1
+
+            this.opts = this.slots.map((data, i) => {
                 const slot = this.$refs.slots[i]
-                const choice = Math.floor( Math.random() * data.items.length )
-                console.log("choice", i, data.items[choice])
-        
+                const choice = Math.floor(Math.random() * data.items.length)
+                console.log('choice', i, data.items[choice])
+
                 const opts = {
                     el: slot.querySelector('.slot__wrap'),
                 finalPos: choice * 180,
                 startOffset: 2000 + Math.random() * 500 + i * 500,
                 height: data.items.length * 180,
                 duration: 3000 + i * 700, // milliseconds
-                isFinished: false,
+                isFinished: false
                 }
                 this.elegidos.push(this.juego[choice])
                 return opts
-                
             })
-            next( this.animate )
-        }, 
-        animate: function(timestamp) {
-            
+            next(this.animate)
+        },
+        animate: function (timestamp) {
             if (this.startedAt == null) {
                 this.startedAt = timestamp
             }
-            
+
             const timeDiff = timestamp - this.startedAt
-            
-            this.opts.forEach( opt => {
-            
+
+            this.opts.forEach(opt => {
                 if (opt.isFinished) {
                     return
                 }
-                    
+
                 const timeRemaining = Math.max(opt.duration - timeDiff, 0)
                 const power = 3
-                const offset = ( Math.pow(timeRemaining, power) / Math.pow(opt.duration, power) ) * opt.startOffset
-                
+                const offset = (Math.pow(timeRemaining, power) / Math.pow(opt.duration, power)) * opt.startOffset
+
                 // negative, such that slots move from top to bottom
                 const pos = -1 * Math.floor((offset + opt.finalPos) % opt.height)
-                
-                opt.el.style.transform = "translateY(" + pos + "px)"
-                
-                if ( timeDiff > opt.duration ) {
+
+                opt.el.style.transform = 'translateY(' + pos + 'px)'
+
+                if (timeDiff > opt.duration) {
                 console.log('finished', opt, pos, opt.finalPost)
                     opt.isFinished = true
                 }
-                
             })
-        
-            if (this.opts.every( o => o.isFinished )) {
+
+            if (this.opts.every(o => o.isFinished)) {
                 this.opts = null
                 this.startedAt = null
                 console.log('finished')
                 console.log(this.elegidos)
-                this.darPremio();
-            } 
-            else {
-                next( this.animate )
+                this.darPremio()
+            } else {
+                next(this.animate)
             }
         },
-        darPremio: function() {
-
-            //Gana sin jackpot
-            if(this.elegidos[0].nombre == this.elegidos[1].nombre  && this.elegidos[1].nombre == this.elegidos[2].nombre && !this.elegidos[0].esJackpot)
-            {
-                this.creditoAcumulado += this.tamanioApuesta * this.elegidos[0].valor;
-                this.partidasGanadas += 1;
-                this.jackpotAcumulado += this.tamanioApuesta / 7;
+        darPremio: function () {
+            // Gana sin jackpot
+            if (this.elegidos[0].nombre == this.elegidos[1].nombre && this.elegidos[1].nombre == this.elegidos[2].nombre && !this.elegidos[0].esJackpot) {
+                this.creditoAcumulado += this.tamanioApuesta * this.elegidos[0].valor
+                this.partidasGanadas += 1
+                this.jackpotAcumulado += this.tamanioApuesta / 7
                 alert('GANASTE $ ' + this.tamanioApuesta * this.elegidos[0].valor)
+            } else if (this.elegidos[0].nombre == this.elegidos[1].nombre && this.elegidos[1].nombre == this.elegidos[2].nombre && this.elegidos[0].esJackpot) {
+                this.creditoAcumulado += this.jackpotAcumulado
+                this.partidasGanadas += 1
+                this.jackpots += 1
+                this.jackpotAcumulado = 5000
+                alert('FELICITACIONES!!! HAZ GANADO EL JACKPOT ACUMULADO DE $ ' + this.jackpotAcumulado)
             }
-            else if(this.elegidos[0].nombre == this.elegidos[1].nombre  && this.elegidos[1].nombre == this.elegidos[2].nombre && this.elegidos[0].esJackpot)
-            {
-                this.creditoAcumulado += this.jackpotAcumulado;
-                this.partidasGanadas += 1;
-                this.jackpots += 1;
-                this.jackpotAcumulado = 5000;
-                alert('FELICITACIONES!!! HAZ GANADO EL JACKPOT ACUMULADO DE $ ' + this.jackpotAcumulado)                
-            }
-            console.log("premio: " + this.creditoAcumulado)
-            console.log(this.elegidos[0].nombre == this.elegidos[1].nombre == this.elegidos[2].nombre);
-            console.log("tamanio apuesta: " + this.tamanioApuesta)
-            console.log(this.elegidos[0].nombre+ ' ' + this.elegidos[1].nombre + ' ' + this.elegidos[2].nombre);
-            console.log(this.juego);
+            console.log('premio: ' + this.creditoAcumulado)
+            console.log(this.elegidos[0].nombre == this.elegidos[1].nombre == this.elegidos[2].nombre)
+            console.log('tamanio apuesta: ' + this.tamanioApuesta)
+            console.log(this.elegidos[0].nombre + ' ' + this.elegidos[1].nombre + ' ' + this.elegidos[2].nombre)
+            console.log(this.juego)
         },
-        elegirJuego: function(numeroJuego) {
-            let iconos;
-            let juegoNuevo;
-            switch(numeroJuego)
-            {
+        elegirJuego: function (numeroJuego) {
+            let iconos
+            let juegoNuevo
+            switch (numeroJuego) {
                 case 1:
-                    iconos = [juego1[0].imagen,juego1[1].imagen,juego1[2].imagen];
-                    juegoNuevo = [juego1[0],juego1[1],juego1[2]];
-                    break;
+                    iconos = [juego1[0].imagen, juego1[1].imagen, juego1[2].imagen]
+                    juegoNuevo = [juego1[0], juego1[1], juego1[2]]
+                    break
                 case 2:
-                    iconos = [juego2[0].imagen,juego2[1].imagen,juego2[2].imagen,juego2[3].imagen];
-                    juegoNuevo = [juego2[0],juego2[1],juego2[2],juego2[3]];
-                    break;
+                    iconos = [juego2[0].imagen, juego2[1].imagen, juego2[2].imagen, juego2[3].imagen]
+                    juegoNuevo = [juego2[0], juego2[1], juego2[2], juego2[3]]
+                    break
                 case 3:
-                    iconos = [juego3[0].imagen,juego3[1].imagen,juego3[2].imagen,juego3[3].imagen,juego3[4].imagen];
-                    juegoNuevo = [juego3[0],juego3[1],juego3[2],juego3[3],juego3[4]];
-                    break;
+                    iconos = [juego3[0].imagen, juego3[1].imagen, juego3[2].imagen, juego3[3].imagen, juego3[4].imagen]
+                    juegoNuevo = [juego3[0], juego3[1], juego3[2], juego3[3], juego3[4]]
+                    break
                 case 4:
-                    iconos = [juego4[0].imagen, juego4[1].imagen, juego4[2].imagen, juego4[3].imagen, juego4[4].imagen, juego4[5].imagen];
-                    juegoNuevo = [juego4[0],juego4[1],juego4[2],juego4[3],juego4[4],juego4[5]]
+                    iconos = [juego4[0].imagen, juego4[1].imagen, juego4[2].imagen, juego4[3].imagen, juego4[4].imagen, juego4[5].imagen]
+                    juegoNuevo = [juego4[0], juego4[1], juego4[2], juego4[3], juego4[4], juego4[5]]
                 }
-            this.slots[0].items = iconos;
-            this.slots[1].items = iconos;
-            this.slots[2].items = iconos;
-            this.juego = juegoNuevo;
-            console.log("asdasd");
-            console.log(iconos);
-            console.log(this.slots);
+            this.slots[0].items = iconos
+            this.slots[1].items = iconos
+            this.slots[2].items = iconos
+            this.juego = juegoNuevo
+            console.log('asdasd')
+            console.log(iconos)
+            console.log(this.slots)
             return iconos
-        } 
-        
+        }
+
     }
-});
+})
